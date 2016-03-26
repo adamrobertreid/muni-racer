beb7e8195bb78f8a5f4655367162795defa3ad03
adamrobertreid <WDI_GA@Adams-MacBook-Pro-3.local>
2016-03-26 14:37:19 -0700

    I have OOP with keypress working only for one button

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,32 @@
 console.log('Sanity Check: JS is working!');
+//variables for players
+
+
 
 $(document).on("ready", function() {
 
+  var playerOne = new Player (81,0);
+  var playerTwo = new Player (80,0);
 
-//create divs that will appear in divs to race
+  playerOne.move(); // calls player one to move left
+  playerTwo.move(); // calls player two to move left
 
-
+  console.log(playerOne);
+  console.log(playerTwo);
 
 
 });
+
+function Player (keybutton,win){
+    this.keybutton = keybutton;
+    this.win = win;
+    this.move = function(){
+      $(window).on('keydown',function moveRight(event) {
+        var car1 = ("#player1");
+        var car2 = ("")
+        if (event.keyCode === keybutton){
+          $(cars).animate({ "left": "+=50px" }, "slow" );
+        }
+      });
+    };
+  }
