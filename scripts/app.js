0bf8db1d38b70d54520d9ab2d91a862d1a3c4a79
adamrobertreid <WDI_GA@Adams-MacBook-Pro-3.local>
2016-03-26 15:49:33 -0700

    almost lost it all, new start on next update

--- scripts/app.js
+++ scripts/app.js
@@ -4,9 +4,13 @@ console.log('Sanity Check: JS is working!');
 
 
 $(document).on("ready", function() {
+  var source   = $("#raceCars").html();
+  var template = Handlebars.compile(source);
 
-  var playerOne = new Player (81,0);
-  var playerTwo = new Player (80,0);
+  var playerOne = new Player ("playerOne",81,0);
+  var playerTwo = new Player ("playerTwo",80,0);
+
+  var renderHTML = template({player: playerOne});
 
   playerOne.move(); // calls player one to move left
   playerTwo.move(); // calls player two to move left
@@ -14,18 +18,19 @@ $(document).on("ready", function() {
   console.log(playerOne);
   console.log(playerTwo);
 
-
+$('#path').append(renderHTML);
+console.log(renderHTML)
 });
 
-function Player (keybutton,win){
+function Player (id,keybutton,win){
+    this.id = id;
     this.keybutton = keybutton;
     this.win = win;
     this.move = function(){
       $(window).on('keydown',function moveRight(event) {
-        var car1 = ("#player1");
-        var car2 = ("")
+
         if (event.keyCode === keybutton){
-          $(cars).animate({ "left": "+=50px" }, "slow" );
+          $('#'+id).animate({ "left": "+=20px" }, 0 );
         }
       });
     };
