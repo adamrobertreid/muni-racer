3e5e6df1a7fb6770ced7efe05cd3a2a1d00ff0b6
adamrobertreid <WDI_GA@Adams-MacBook-Pro-3.local>
2016-03-26 16:45:45 -0700

    working on new method to find winner, counting number of clicks

--- scripts/app.js
+++ scripts/app.js
@@ -6,32 +6,21 @@ console.log("JS has loaded!");
 $(document).ready(function() {
   console.log("the document is ready");
 
-  var playerOne = new Player (81, 0);
-  var playerTwo = new Player (80, 0);
 
-  console.log(playerTwo);
-  console.log(playerOne);
-
-  playerOne.move();
-  playerTwo.move();
-
-  function Player(keybutton, win) {
-    this.keybutton = keybutton;
-    this.win = function(){
-      var keysCounted = 0;
-    }; //create for loop to declare winner
-    this.move = function() {
+      function race() {
       $(window).on('keydown', function moveRight(event) {
           if(event.keyCode === 81) {
             $('#player1').animate({ 'left': '+=10px' }, 0);
-          } else if(event.keyCode === 80) {
+            if('keydown'<= 34);
+            alert("winner");
+            } else if(event.keyCode === 80) {
             $('#player2').animate({ 'left': '+=10px' }, 0);
           }
         });
-      };
-  }
+      }
 
 
+  race();
 
 });
 
