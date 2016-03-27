3cb6f5c52301ea53b978078ef478c904e92d223a
adamrobertreid <WDI_GA@Adams-MacBook-Pro-3.local>
2016-03-26 23:22:49 -0700

    Alert player win has been created, feature working next step make key press stop

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,25 @@
 console.log("JS has loaded!");
 
-//GLOBAL VARIABLES
+
 
 
 $(document).ready(function() {
   console.log("the document is ready");
 
-
+//Function will alert winner once either players div(car) reaches end of track.
+  function winner() {
+      if ($('#player1').css('left') === '650px') {
+        alert("Player One Wins!!!");
+      }else if ($('#player2').css('left') === '650px') {
+        alert("Player Two Wins!!!");
+      }
+    }
+//Function event listener works by moving div after certain keys are pressed
       function race() {
       $(window).on('keydown', function moveRight(event) {
+          if(winner());
           if(event.keyCode === 81) {
             $('#player1').animate({ 'left': '+=10px' }, 0);
-            if('keydown'<= 34);
-            alert("winner");
             } else if(event.keyCode === 80) {
             $('#player2').animate({ 'left': '+=10px' }, 0);
           }
@@ -23,6 +30,3 @@ $(document).ready(function() {
   race();
 
 });
-
-
-//34 keypresses to get to the end of the track.
