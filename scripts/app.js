469ab0a1b73e2d9bcdb4818662214a5a55d8d518
adamrobertreid <WDI_GA@Adams-MacBook-Pro-3.local>
2016-03-26 23:46:42 -0700

    working on preventing keydown after win

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,27 @@
 console.log("JS has loaded!");
 
 
-
+var count1 = 0;
+var count2 = 0;
 
 $(document).ready(function() {
   console.log("the document is ready");
 
+$(".count1").text('Player One ' + count1);
+$(".count2").text('Player Two ' + count2);
+
+
 //Function will alert winner once either players div(car) reaches end of track.
   function winner() {
       if ($('#player1').css('left') === '650px') {
+        $('keydown') // working on solution to prevent futher moves from keydown
         alert("Player One Wins!!!");
+        count1++;
+        console.log("counter1", count1);
       }else if ($('#player2').css('left') === '650px') {
         alert("Player Two Wins!!!");
+        count2++;
+        console.log("counter 2", count2);
       }
     }
 //Function event listener works by moving div after certain keys are pressed
@@ -25,6 +35,7 @@ $(document).ready(function() {
           }
         });
       }
+
 
 
   race();
