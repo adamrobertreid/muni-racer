2ec33a5128a99cecc93213ef1d30e694674e9925
adamrobertreid <WDI_GA@Adams-MacBook-Pro-3.local>
2016-03-27 09:32:31 -0700

    Game works with base requirements except OOP, if time and sanity allows add OOP for game track and palyer inputs

--- scripts/app.js
+++ scripts/app.js
@@ -1,43 +1,35 @@
 console.log("JS has loaded!");
 
-
-var count1 = 0;
-var count2 = 0;
+var keyQ = 81; //key assigned to player one is Q
+var keyP = 80;//key assigned to player two is P
 
 $(document).ready(function() {
   console.log("the document is ready");
 
-$(".count1").text('Player One ' + count1);
-$(".count2").text('Player Two ' + count2);
-
+  race();
+  $(".btn").on("click", function resetWholeGame(event){
+    window.location.reload();
+});
 
 //Function will alert winner once either players div(car) reaches end of track.
   function winner() {
       if ($('#player1').css('left') === '650px') {
-        $('keydown') // working on solution to prevent futher moves from keydown
         alert("Player One Wins!!!");
-        count1++;
-        console.log("counter1", count1);
       }else if ($('#player2').css('left') === '650px') {
         alert("Player Two Wins!!!");
-        count2++;
-        console.log("counter 2", count2);
+
       }
     }
+
 //Function event listener works by moving div after certain keys are pressed
       function race() {
       $(window).on('keydown', function moveRight(event) {
           if(winner());
-          if(event.keyCode === 81) {
+          if(event.keyCode === keyQ) {
             $('#player1').animate({ 'left': '+=10px' }, 0);
-            } else if(event.keyCode === 80) {
+          } else if(event.keyCode === keyP) {
             $('#player2').animate({ 'left': '+=10px' }, 0);
           }
-        });
-      }
-
-
-
-  race();
-
+      });
+    }
 });
